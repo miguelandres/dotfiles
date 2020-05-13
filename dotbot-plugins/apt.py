73685bcf46df65fe2fbe452e5b710bccf6dd6656
Miguel Barreto <miguelandres@users.noreply.github.com>
2020-05-12 21:40:18 -0400

    Add snap/apt plugin for dotbot, import brew plugin
    Clean up all related directives in configs and clean up shell directives
    as well since they were very very polluted with default settings.

---
+++ dotbot-plugins/apt.py
@@ -0,0 +1,28 @@
+from subprocess import CalledProcessError, check_call
+from typing import Any, List, Sequence
+
+import dotbot
+
+
+class Apt(dotbot.Plugin):
+    def can_handle(self, directive: str) -> bool:
+        return directive == "apt"
+
+    def handle(self, directive: str, packages: List[str]) -> bool:
+        success = self._run(["sudo","apt", "update"], "Updating APT, may require sudoing") \
+                  and self._run(["sudo", "apt", "install", "-y"] + packages,
+                                "Installing: {}".format(", ".join(packages)))
+
+        if success:
+            self._log.info("APT packages installed successfully")
+
+        return success
+
+    def _run(self, command: Sequence[Any], log_message: str) -> bool:
+        self._log.lowinfo(log_message)
+        try:
+            check_call(command)
+            return True
+        except CalledProcessError as e:
+            self._log.error(e)
+            return False
---
+++ dotbot-plugins/snap.py
@@ -0,0 +1,30 @@
+from subprocess import CalledProcessError, check_call
+from typing import Any, List, Sequence
+
+import dotbot
+
+
+class Snap(dotbot.Plugin):
+    def can_handle(self, directive: str) -> bool:
+        return directive == "snap"
+
+    def handle(self, directive: str, packages: List[str]) -> bool:
+        self._log.info("Snap Installing {}".format(", ".join(packages)))
+        success = True
+        for pkg in packages:
+            success = success and self._run(
+                ["sudo", "snap", "install", pkg], "Installing {}. May need to sudo.".format(pkg))
+
+        if success:
+            self._log.info("SNAP packages installed successfully")
+
+        return success
+
+    def _run(self, command: Sequence[Any], log_message: str) -> bool:
+        self._log.lowinfo(log_message)
+        try:
+            check_call(command)
+            return True
+        except CalledProcessError as e:
+            self._log.error(e)
+            return False
